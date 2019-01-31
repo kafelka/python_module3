92938c0cb8ea0b6b7cf35a82ae008af4ddb06bbe
Mag Kafel <na.hasiok@gmail.com>
2019-01-31 10:00:37 +0000

    updated ch04

--- Chapter_5_External_testing_tools/test_calculatorApp.py
+++ Chapter_5_External_testing_tools/test_calculatorApp.py
@@ -3,20 +3,26 @@ from calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        calc = Calculator()
-        self.assertEqual(6, calc.add(3,3))
+        self.calc = Calculator()
+        self.assertEqual(6.5, self.calc.add(3,3.5))
     def test_subtr(self):
-        calc = Calculator()
-        self.assertEqual(-1, calc.subtract(2,3))
+        self.calc = Calculator()
+        self.assertEqual(-1, self.calc.subtract(2,3))
     def test_divide(self):
-        calc = Calculator()
-        self.assertEqual(1, calc.divide(1,1))
+        self.calc = Calculator()
+        self.assertEqual(1, self.calc.divide(1,1))
     def test_multiply(self):
-        calc = Calculator()
-        self.assertEqual(6, calc.multiply(2,3))
-    def test_calc_returns_error_msg_if_both_args_not_numbers(self):
-        calc = Calculator()
-        self.assertRaises(ValueError, calc.add, "two", "three")
+        self.calc = Calculator()
+        self.assertEqual(6, self.calc.multiply(2,3))
+#    def test_calc_returns_error_msg_if_both_args_not_numbers(self):
+#        self.calc = Calculator()
+#        self.assertRaises(ValueError, self.calc.add, "two", "three")
+#    def test_calc_returns_error_if_x_arg_not_number(self):
+#        self.calc = Calculator()
+#        self.assertRaises(ValueError, self.calc.add, "two", 3)
+#    def test_calc_returns_error_if_y_arg_not_number(self):
+#        self.calc = Calculator()
+#        self.assertRaises(ValueError, self.calc.add, 2, "three")
     
 if __name__ == "__main__":
     unittest.main()
